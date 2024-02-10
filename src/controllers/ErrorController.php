0eb41ae05651ccb75240c5f628aa6caa75f595c1
RodrigoFlorencio <rhodrigoflorencio@yahoo.com>
2024-02-10 17:45:48 -0300

    Update ErrorController.php

--- src/controllers/ErrorController.php
+++ src/controllers/ErrorController.php
@@ -6,8 +6,7 @@ use \core\Controller;
 class ErrorController extends Controller {
 
     public function index() {
-        echo "Deu erro";
-        //$this->render('404');
+        $this->render('404');
     }
 
 }
